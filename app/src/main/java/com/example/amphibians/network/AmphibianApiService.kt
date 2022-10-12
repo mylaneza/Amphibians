fc92c10b4383cd804ad11cbcdee31fac8ad3b3df
Ignacio Gonzalez <mylaneza@gmail.com>
2022-10-12 13:06:52 -0500

    Changes to pass tests

--- app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
+++ app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -23,8 +23,8 @@ import retrofit2.create
 import retrofit2.http.GET
 
 // TODO: Create a property for the base URL provided in the codelab
-private const val BASE_URL = "https://developer.android.com/courses/pahtways/android-basics-kotlin-unit-4-pathway-2/"
 
+private const val BASE_URL = "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
 // TODO: Build the Moshi object with Kotlin adapter factory that Retrofit will be using to parse JSON
 private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 // TODO: Build a Retrofit object with the Moshi converter
@@ -34,7 +34,7 @@ private val retrofit = Retrofit.Builder()
     .build()
 interface AmphibianApiService {
     // TODO: Declare a suspended function to get the list of amphibians
-    @GET("amphibians")
+    @GET("android-basics-kotlin-unit-4-pathway-2-project-api.json")
     suspend fun getAmphibians() : List<Amphibian>
 }
 
